98e4b87d6322ddc30a7faf8cdadde65bb60ec823
Kevin Tun <kevin.tun@northwestern.edu>
2024-05-22 15:53:57 -0500

    feat(types): add 'type' and 'date' fields to the GeneOntologyAnnotation type in GeneSummaryQuery
    The 'type' and 'date' fields were added to the GeneOntologyAnnotation type in the GeneSummaryQuery to provide additional information for each annotation. This enhancement improves the completeness and usefulness of the data returned by the query.

--- types/index.ts
+++ types/index.ts
@@ -1071,7 +1071,7 @@ export type GeneSummaryQueryVariables = Exact<{
 }>;
 
 
-export type GeneSummaryQuery = { __typename?: 'Query', geneGeneralInformation?: { __typename?: 'GeneGeneralInfo', name_description: Array<string>, alt_gene_name: Array<string>, gene_product: string, alt_protein_names: Array<string>, description: string } | null, geneOntologyAnnotation?: Array<{ __typename?: 'GOAnnotation', id: string, go_term: string, evidence_code: string, with?: Array<{ __typename?: 'With', id: string, db: string, name: string }> | null, extensions?: Array<{ __typename?: 'Extension', id: string, db: string, relation: string, name: string }> | null }> | null };
+export type GeneSummaryQuery = { __typename?: 'Query', geneGeneralInformation?: { __typename?: 'GeneGeneralInfo', name_description: Array<string>, alt_gene_name: Array<string>, gene_product: string, alt_protein_names: Array<string>, description: string } | null, geneOntologyAnnotation?: Array<{ __typename?: 'GOAnnotation', id: string, type: string, date: string, go_term: string, evidence_code: string, with?: Array<{ __typename?: 'With', id: string, db: string, name: string }> | null, extensions?: Array<{ __typename?: 'Extension', id: string, db: string, relation: string, name: string }> | null }> | null };
 
 export type GeneOntologyAnnotationQueryVariables = Exact<{
   gene: Scalars['String']['input'];
@@ -1653,6 +1653,8 @@ export const GeneSummaryDocument = gql`
   }
   geneOntologyAnnotation(gene: $gene) {
     id
+    type
+    date
     go_term
     evidence_code
     with {
